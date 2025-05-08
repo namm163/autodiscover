4e4f01b392c604968de1682bc54937ade7d76164
q00534820 <qiuyueqiong@huawei.com>
2025-05-08 20:49:04 +0800

    update autodiscover.xml

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ export default {
 				const xml = `<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006">
   <Response xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a">
     <User>
-      <DisplayName>users</DisplayName>
+      <DisplayName>bob</DisplayName>
       <LegacyDN>
-        /o=example.com/ou=Exchange/cn=Recipients/cn=a55be5ac09bb4734812572b46bfc57d4-users</LegacyDN>
-      <AutoDiscoverSMTPAddress>boby@534820.xyz</AutoDiscoverSMTPAddress>
+        /o=197955.xyz/ou=Exchange/cn=Recipients/cn=a55be5ac09bb4734812572b46bfc57d4-users</LegacyDN>
+      <AutoDiscoverSMTPAddress>boby@197955.xyz</AutoDiscoverSMTPAddress>
       <DeploymentId>7d3020ab-d683-4dd2-b5bd-2a82b8e716b4</DeploymentId>
     </User>
     <Account>
@@ -23,9 +23,9 @@ export default {
         <Type>SMTP</Type>
         <Server>smtp.sparkspace.huaweicloud.com</Server>
         <Port>465</Port>
-        <LoginName>bob@534820.xyz</LoginName>
+        <LoginName>bob@197955.xyz</LoginName>
         <DomainRequired>On</DomainRequired>
-        <DomainName>534820.xyz</DomainName>
+        <DomainName>197955.xyz</DomainName>
         <SPA>Off</SPA>
         <SSL>On</SSL>
         <AuthRequired>On</AuthRequired>
@@ -34,9 +34,9 @@ export default {
         <Type>IMAP</Type>
         <Server>imap.sparkspace.huaweicloud.com</Server>
         <Port>993</Port>
-        <LoginName>bob@534820.xyz</LoginName>
+        <LoginName>bob@197955.xyz</LoginName>
         <DomainRequired>On</DomainRequired>
-        <DomainName>534820.xyz</DomainName>
+        <DomainName>197955.xyz</DomainName>
         <SPA>Off</SPA>
         <SSL>On</SSL>
         <AuthRequired>On</AuthRequired>
@@ -45,9 +45,9 @@ export default {
         <Type>POP3</Type>
         <Server>pop3.sparkspace.huaweicloud.com</Server>
         <Port>995</Port>
-        <LoginName>bob@534820.xyz</LoginName>
+        <LoginName>bob@197955.xyz</LoginName>
         <DomainRequired>On</DomainRequired>
-        <DomainName>534820.xyz</DomainName>
+        <DomainName>197955.xyz</DomainName>
         <SPA>Off</SPA>
         <SSL>On</SSL>
         <AuthRequired>On</AuthRequired>
